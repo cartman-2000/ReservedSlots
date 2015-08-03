ceacda3821add537a8932e5e0f86b89244059681
cartman-2000 <cartman-2000@cartman-2000.net>
2015-08-03 16:34:47 -0700

    Initial commit.

---
+++ ReservedSlots.cs
@@ -0,0 +1,100 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Permissions;
+using SDG.Unturned;
+using Steamworks;
+using Rocket.Core.Serialisation;
+using Rocket.Core;
+using Rocket.API;
+
+namespace ReservedSlots
+{
+    public class RocketPlayer : IRocketPlayer
+    {
+        public string Id
+        {
+            get; set;
+        }
+
+        public string DisplayName
+        {
+            get; set;
+        }
+    }
+
+    public class ReservedSlots : RocketPlugin<ReservedSlotsConfig>
+    {
+        public ReservedSlots Instance;
+
+        protected override void Load()
+        {
+            Instance = this;
+            UnturnedPermissions.OnJoinRequested += Events_OnJoinRequested;
+            Logger.Log(string.Format("Reserved Slots enabled: {0}, Count: {1}, Allowfill: {2}.", Instance.Configuration.Instance.ReservedSlotEnable, Instance.Configuration.Instance.ReservedSlotCount, Instance.Configuration.Instance.AllowFill));
+        }
+
+        private void Events_OnJoinRequested(CSteamID CSteamID, ref ESteamRejection? rejectionReason)
+        {
+            if (Instance.Configuration.Instance.ReservedSlotEnable && Instance.Configuration.Instance.ReservedSlotCount > 0 && Instance.Configuration.Instance.Groups != null && Instance.Configuration.Instance.Groups.Count > 0)
+            {
+                int numPlayers = Steam.Players.Count;
+                int maxPlayers = Steam.maxPlayers;
+                switch (Instance.Configuration.Instance.AllowFill)
+                {
+                    case false:
+                        // Don't allow the slots to fill.
+                        if (numPlayers + Instance.Configuration.Instance.ReservedSlotCount >= maxPlayers)
+                        {
+                            if (!CheckReserved(CSteamID))
+                            {
+#if DEBUG
+                                Logger.Log(string.Format("Rejected: {0}, curplayercount: {1}", CSteamID.ToString(), numPlayers.ToString()));
+#endif
+                                rejectionReason = ESteamRejection.SERVER_FULL;
+                            }
+                        }
+                        break;
+                    default:
+                        // Allow them to fill.
+                        foreach (SteamPlayer player in Steam.Players)
+                        {
+                            if (CheckReserved(player.SteamPlayerID.CSteamID))
+                            {
+                                numPlayers--;
+                            }
+                        }
+                        if (numPlayers + Instance.Configuration.Instance.ReservedSlotCount >= maxPlayers)
+                        {
+                            if (!CheckReserved(CSteamID))
+                            {
+#if DEBUG
+                                Logger.Log(string.Format("Rejected: {0}, curplayercount: {1}, adjusted: {2}", CSteamID.ToString(), Steam.Players.Count.ToString(), numPlayers.ToString()));
+#endif
+                                rejectionReason = ESteamRejection.SERVER_FULL;
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+
+        private bool CheckReserved(CSteamID CSteamID)
+        {
+            if (SteamAdminlist.checkAdmin(CSteamID))
+            {
+                return true;
+            }
+            else
+            {
+                foreach (RocketPermissionsGroup group in R.Permissions.GetGroups(new RocketPlayer() { Id = CSteamID.ToString(), DisplayName = "" }, true))
+                {
+                    if (Instance.Configuration.Instance.Groups.Contains(group.Id))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ ReservedSlotsConfig.cs
@@ -0,0 +1,27 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace ReservedSlots
+{
+    public class ReservedSlotsConfig : IRocketPluginConfiguration
+    {
+        public bool ReservedSlotEnable;
+        [XmlArray("Groups"), XmlArrayItem(ElementName = "Group")]
+        public List<string> Groups;
+        public int ReservedSlotCount;
+        public bool AllowFill;
+
+        public void LoadDefaults()
+        {
+            ReservedSlotEnable = true;
+            Groups = new List<string>
+            {
+                "moderator",
+                "admin"
+            };
+            ReservedSlotCount = 2;
+            AllowFill = false;
+        }
+    }
+}
